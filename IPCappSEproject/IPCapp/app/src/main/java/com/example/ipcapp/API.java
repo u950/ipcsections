1410f81f2d7755be050a8f6fbef1224a2894d379
UDAY BIDDIKA <103553174+u950@users.noreply.github.com>
2023-08-25 03:36:50 +0530

    Update API.java

--- IPCappSEproject/IPCapp/app/src/main/java/com/example/ipcapp/API.java
+++ IPCappSEproject/IPCapp/app/src/main/java/com/example/ipcapp/API.java
@@ -2,6 +2,6 @@ package com.example.ipcapp;
 
 public class API {
     public static String API_URL = "https://api.openai.com/v1/completions";
-    public static String API = "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";
+    public static String API = "your_api_key";
 
 }
